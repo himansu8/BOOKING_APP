235d2d387ae252e4bceba5800155bd52b1b6d8d9
himansu8 <himansusekharsahu50@gmail.com>
2024-03-17 00:52:49 +0530

    single page

--- admin/src/components/chart/Chart.jsx
+++ admin/src/components/chart/Chart.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import "./chart.scss"
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { AreaChart, Area, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
-function Chart() {
+function Chart({aspect, title}) {
   const data = [
     { name: "January", Total: 1200 },
     { name: "February", Total: 2100 },
@@ -15,8 +15,8 @@ function Chart() {
 
   return (
     <div className='chart'>
-      <div className="title">Last 6 Months (Revenue) </div>
-      <ResponsiveContainer width="100%" aspect={2 / 1}>
+      <div className="title">{title} </div>
+      <ResponsiveContainer width="100%" aspect={aspect}>
         <AreaChart width={730} height={250} data={data}
           margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
           <defs>
--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import "./home.scss"
 import Widget from '../../components/widget/Widget'
 import Featured from '../../components/featured/Featured'
 import Chart from '../../components/chart/Chart'
+import Table from '../../components/table/Table'
 function Home() {
     return (
         <div className='home'>
@@ -19,7 +20,11 @@ function Home() {
                 </div>
                 <div className="charts">
                     <Featured/>
-                    <Chart/>
+                    <Chart title="Last 6 Months (Revenue)" aspect = {2 / 1}/>
+                </div>
+                <div className="listContainer">
+                    <div className="listTitle">Latest Transaction</div>
+                    <Table />
                 </div>
             </div>
         </div>
